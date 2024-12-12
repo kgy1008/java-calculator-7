d01ca9bea9e6a0350fe5b5c2b6c882e771520c93
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 13:48:27 +0900

    fix: 빈 문자열은 0이 되도록 처리

--- src/main/java/calculator/domain/NumberParser.java
+++ src/main/java/calculator/domain/NumberParser.java
@@ -8,15 +8,12 @@ public class NumberParser {
     public List<Number> parse(final String target, final List<String> delimiters) {
         String regex = makeRegex(delimiters);
         return Arrays.stream(target.split(regex))
+                .filter(number -> !number.isBlank())
                 .map(number -> new Number(number.trim()))
                 .toList();
     }
 
     private String makeRegex(final List<String> delimiters) {
-        StringBuilder regex = new StringBuilder();
-        for (String delimiter : delimiters) {
-            regex.append(delimiter).append("|");
-        }
-        return regex.substring(0, regex.length() - 1);
+        return String.join("|", delimiters);
     }
 }

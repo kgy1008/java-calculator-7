ef6deaae05c046bd6677fe98fbdb7722ad7fdf39
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 12:44:16 +0900

    feat: 구분자 파싱 후, 구분자 목록 반환

--- src/main/java/calculator/domain/DelimiterParser.java
+++ src/main/java/calculator/domain/DelimiterParser.java
@@ -1,4 +1,27 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DelimiterParser {
+
+    private static final List<String> DEFAULT_DELIMITERS = List.of(",", ":");
+    private static final String PREFIX = "//";
+    private static final String SUFFIX = "\\n";
+
+    public List<String> parse(final String input) {
+        List<String> delimiters = new ArrayList<>(DEFAULT_DELIMITERS);
+        if (isContainCustomDelimiter(input)) {
+            String customDelimiter = input.substring(PREFIX.length(), input.indexOf(SUFFIX));
+            delimiters.add(customDelimiter);
+        }
+        return delimiters;
+    }
+
+    private boolean isContainCustomDelimiter(final String input) {
+        if (input.contains(PREFIX) && input.contains(SUFFIX)) {
+            return true;
+        }
+        return false;
+    }
 }

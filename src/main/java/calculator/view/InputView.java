4aeb2775a1360cbf9378ac7e36bc6b358d934e7c
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 12:30:21 +0900

    feat: 입력 안내 메세지 작성

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String inputTarget() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         validate(input);
         return input;

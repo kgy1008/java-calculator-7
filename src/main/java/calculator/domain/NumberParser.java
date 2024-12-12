eb65597006236a76b57b3670d2c3a01879ef47ce
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 13:06:37 +0900

    test: 숫자 파싱 로직 구현

--- src/main/java/calculator/domain/NumberParser.java
+++ src/main/java/calculator/domain/NumberParser.java
@@ -1,4 +1,22 @@
 package calculator.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NumberParser {
+
+    public List<Number> parse(final String target, final List<String> delimiters) {
+        String regex = makeRegex(delimiters);
+        return Arrays.stream(target.split(regex))
+                .map(number -> new Number(number.trim()))
+                .toList();
+    }
+
+    private String makeRegex(final List<String> delimiters) {
+        StringBuilder regex = new StringBuilder();
+        for (String delimiter : delimiters) {
+            regex.append(delimiter).append("|");
+        }
+        return regex.substring(0, regex.length() - 1);
+    }
 }
